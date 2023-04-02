eda97163e844dcf3415c79e51d77082644c5ac2f
sunblock99 <sunblock99@kakao.com>
2023-04-02 20:40:49 +0900

    blockchain 만들기 완성 !

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,52 @@
-import { init, exit} from "myPackage";
+import crypto from "crypto";
 
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
+class Block implements BlockShape {
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`;
+    return crypto.createHash("sha256").update(toHash).digest("hex");
+  }
+}
+class Blockchain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+  private getPrevHash() {
+    if (this.blocks.length === 0) return "";
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+  public addBlock(data: string) {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  }
+  public getBlocks() {
+    return [...this.blocks];
+  }
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock("First block");
+blockchain.addBlock("Second block");
+blockchain.addBlock("Third block");
+blockchain.addBlock("Fourth block");
+
+console.log(blockchain.getBlocks());
--- src/myPackage.d.ts
+++
@@ -1,9 +0,0 @@
-interface Config{
-    url: string;
-}
-
-
-declare module "myPackage"{
-    function init(config:Config):boolean
-    function exit(code:number):number
-}
